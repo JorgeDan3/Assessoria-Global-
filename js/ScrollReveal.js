270db4e085cf325fb6e50dfc52ae5df597282b80
Daniel F.K <129869423+JorgeDan3@users.noreply.github.com>
2023-08-03 06:41:32 +0200

    Add files via upload

---
+++ js/ScrollReveal.js
@@ -0,0 +1,25 @@
+
+window.sr = ScrollReveal()
+
+
+sr.reveal('.services', {
+    duration: 1000,
+    reset: true,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.main-content', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
+
+sr.reveal('.more-cont', {
+    
+    duration: 1000,
+    reset: true,
+    origin: 'top',
+    distance: '-100px'
+});
+
